882335935e9c223746328c0fac11ac15b358990a
smit312 <smit.coder31@gmail.com>
2022-01-04 11:18:04 +0530

    create react app using CRA and started implementing testing

---
+++ react/client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <button style={{backgroundColor:'red'}}>change to blue</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/client/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('button has correct initial color ', () => {
+ render(<App />);
+  const colorButton = screen.getByRole('button' , {name : 'change to blue'});
+
+  expect(colorButton).toHaveStyle({backgroundColor : 'red'})
+});
+test('')
