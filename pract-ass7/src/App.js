2f4062274189f166e675ac5ef2bb64973737c947
smit312 <smit.coder31@gmail.com>
2022-01-27 16:33:11 +0530

    pract-ass-7

---
+++ pract-ass7/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import * as React from "react";
+import UserData from "./component/UserData";
+
+
+function App() {
+  return(
+  <>
+  <UserData />
+  </>
+  )
+}
+
+export default App;
---
+++ pract-ass7/src/actions/index.js
@@ -0,0 +1,12 @@
+export const FirstPage = () => {
+    return {
+        type : "FIRSTPAGE",
+       
+    }
+}
+export const SecondPage = () => {
+    return {
+        type : "SECONDPAGE",
+        
+    }
+}
