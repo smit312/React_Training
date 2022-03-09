da316202a03c9e5e9ee847e26b5d69af69e81bcc
smit312 <smit.coder31@gmail.com>
2022-03-09 16:02:02 +0530

    complete the functionality and test

--- Testing-library/counterapp/src/Counter/Counter.jsx
+++ Testing-library/counterapp/src/Counter/Counter.jsx
@@ -3,18 +3,42 @@ import "./Counter.css";
 const Counter = () => {
   const [counterValue, setcounterValue] = useState(0);
   const [inputValue, setinputValue] = useState(1);
+
+  const addToCouter = () => {
+    setcounterValue(counterValue + inputValue);
+  };
+  const subtractFromCouter = () => {
+    setcounterValue(counterValue - inputValue);
+  };
+
   return (
     <div>
       <h3 data-testid="header">My Counter</h3>
-      <h2 data-testid="counter">{counterValue}</h2>
-      <button data-testid="sub-btn">-</button>
+      <h2
+        data-testid="counter"
+        className={`${counterValue >= 100 ? "green" : ""}${
+          counterValue <= -100 ? "red" : ""
+        }`}
+      >
+        {counterValue}
+      </h2>
+      <button data-testid="sub-btn" onClick={subtractFromCouter}>
+        -
+      </button>
+
       <input
         data-testid="input"
         type="number"
         value={inputValue}
         className="text-center"
+        onChange={(e) => {
+          setinputValue(parseInt(e.target.value));
+        }}
       />
-      <button data-testid="add-btn">+</button>
+
+      <button data-testid="add-btn" onClick={addToCouter}>
+        +
+      </button>
     </div>
   );
 };
