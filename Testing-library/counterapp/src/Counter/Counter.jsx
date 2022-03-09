8cabfe97269fa3cf11ad0a906800409f5ecbdb5f
smit312 <smit.coder31@gmail.com>
2022-03-09 14:22:03 +0530

    add design and their test

--- Testing-library/counterapp/src/Counter/Counter.jsx
+++ Testing-library/counterapp/src/Counter/Counter.jsx
@@ -1,9 +1,20 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./Counter.css";
 const Counter = () => {
+  const [counterValue, setcounterValue] = useState(0);
+  const [inputValue, setinputValue] = useState(1);
   return (
     <div>
       <h3 data-testid="header">My Counter</h3>
+      <h2 data-testid="counter">{counterValue}</h2>
+      <button data-testid="sub-btn">-</button>
+      <input
+        data-testid="input"
+        type="number"
+        value={inputValue}
+        className="text-center"
+      />
+      <button data-testid="add-btn">+</button>
     </div>
   );
 };
--- Testing-library/counterapp/src/Counter/__test__/Counter.test.js
+++ Testing-library/counterapp/src/Counter/__test__/Counter.test.js
@@ -5,6 +5,30 @@ import "@testing-library/jest-dom/extend-expect";
 
 test("header render with correct text", () => {
   render(<Counter />);
-  const header1 = screen.getByTestId("header");
-  expect(header1.textContent).toBe("My Counter");
+  const headerel = screen.getByTestId("header");
+  expect(headerel.textContent).toBe("My Counter");
+});
+
+test("counter is initially start with text 0", () => {
+  const { getByTestId } = render(<Counter />);
+  const counterel = getByTestId("counter");
+  expect(counterel.textContent).toBe("0");
+});
+
+test("input contains initial value of 1 ", () => {
+  const { getByTestId } = render(<Counter />);
+  const inputEl = getByTestId("input");
+  expect(inputEl.value).toBe("1");
+});
+
+test("add button render with + sign", () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId("add-btn");
+  expect(addBtn.textContent).toBe("+");
+});
+
+test("substract button render with - sign", () => {
+  const { getByTestId } = render(<Counter />);
+  const subBtn = getByTestId("sub-btn");
+  expect(subBtn.textContent).toBe("-");
 });
